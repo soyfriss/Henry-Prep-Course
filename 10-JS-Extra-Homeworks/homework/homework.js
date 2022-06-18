9da3807cb90f2961ee7f181a4f22c379b705e79f
soyfriss <federico.rissone@gmail.com>
2022-06-17 22:31:33 -0300

    Update 10-JS-Extra-Homeworks

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,6 +1,6 @@
 // No cambies los nombres de las funciones.
 
-function deObjetoAmatriz(objeto){
+function deObjetoAmatriz(objeto) {
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
   //Ejemplo: 
@@ -34,10 +34,10 @@ function numberOfCharacters(string) {
   var letra = '';
   var objeto = {};
 
-  for (var i=0; i < string.length; i++) {
+  for (var i = 0; i < string.length; i++) {
     letra = string[i];
-    
-    objeto[letra] = (objeto[letra] || 0) + 1;    
+
+    objeto[letra] = (objeto[letra] || 0) + 1;
   }
 
   return objeto;
@@ -53,7 +53,7 @@ function capToFront(s) {
   var letrasMinusculas = '';
   var letra = '';
 
-  for (var i=0; i < s.length; i++) {
+  for (var i = 0; i < s.length; i++) {
     letra = s[i];
     if (letra === letra.toUpperCase()) {
       // Es mayúscula
@@ -80,7 +80,7 @@ function asAmirror(str) {
   // Invierto la palabra y la agrego a la frase nueva junto con los espacios entra cada palabra
   palabras.forEach(function (palabra, indice) {
     // Invierto los caracteres de la palabra
-    for (var i=palabra.length; i > 0; i--) {
+    for (var i = palabra.length; i > 0; i--) {
       fraseInvertida += palabra[i - 1];
     }
     // Agrego los espacios entre palabras salvo en la última palabra
@@ -89,17 +89,17 @@ function asAmirror(str) {
     }
   });
   return fraseInvertida;
-} 
+}
 
 
-function capicua(numero){
+function capicua(numero) {
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
   var numeroOriginal = '' + numero;
   var numeroInvertido = '';
-  for (var i=numeroOriginal.length; i > 0; i--) {
+  for (var i = numeroOriginal.length; i > 0; i--) {
     numeroInvertido += numeroOriginal[i - 1];
   }
   if (numeroOriginal === numeroInvertido) {
@@ -110,13 +110,13 @@ function capicua(numero){
 }
 
 
-function deleteAbc(cadena){
+function deleteAbc(cadena) {
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
   var nuevaCadena = '';
   var letra = '';
-  for (var i=0; i < cadena.length; i++) {
+  for (var i = 0; i < cadena.length; i++) {
     letra = cadena[i];
     if (letra !== 'a' && letra !== 'b' && letra !== 'c') {
       nuevaCadena += letra;
@@ -130,14 +130,40 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-  arr.sort(function(a, b) {
-    return a.length - b.length;
-  })
-  return arr;
+
+  // Implementación con la función sort()
+  // arr.sort(function(a, b) {
+  //   return a.length - b.length;
+  // })
+  // return arr;
+
+  // Nueva implementación
+  var nuevoArr = [];  // El array ordenado
+  var palabra = '';
+  var posicion = 0;   // la nueva posición que va a tomar cada palabra
+  for (var i = 0; i < arr.length; i++) {
+    palabra = arr[i];
+
+    // Comparo la palabra contra las demás y obtengo la nueva posición
+    posicion = 0;
+    for (var j = 0; j < arr.length; j++) {
+      // Si el tamaño de la palabra es mayor a la que se compara sumo 1 a posición
+      if (palabra.length > arr[j].length) {
+        posicion++;
+      }
+      // Si es igual al tamaño y está más atrás de la que se compara sumo 1 a posición
+      if (palabra.length == arr[j].length && i > j) {
+        posicion++;
+      }
+    }
+
+    nuevoArr[posicion] = palabra;  // asigno la palabra en la nueva posición
+  }
+  return nuevoArr;
 }
 
 
-function buscoInterseccion(arreglo1, arreglo2){
+function buscoInterseccion(arreglo1, arreglo2) {
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
@@ -160,13 +186,13 @@ function buscoInterseccion(arreglo1, arreglo2){
 // --------------------------------
 
 module.exports = {
-   deObjetoAmatriz,
-   numberOfCharacters,
-   capToFront,
-   asAmirror,
-   capicua,
-   deleteAbc,
-   sortArray,
-   buscoInterseccion,
+  deObjetoAmatriz,
+  numberOfCharacters,
+  capToFront,
+  asAmirror,
+  capicua,
+  deleteAbc,
+  sortArray,
+  buscoInterseccion,
 };
 
